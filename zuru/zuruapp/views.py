e943628b03dbfcdad44e936f77aa8b953e0ca7af
cospamental <cospa18@gmail.com>
2018-03-22 18:35:46 +0300

    added home,event views

--- zuru/zuruapp/views.py
+++ zuru/zuruapp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Event,Booking,User,Role
 
 # Create your views here.
-def index(request):
+def home(request):
       '''
       View root page function that returns the index page and its data
       '''
       title = 'Home | ZuruKe'
-      events = Event.query.all()
-      return render('index.html', {"title":title,"events":events})
+      event_list = Event.objects.all()
+
+      return render(request,'index.html', {"title":title,"event_list":event_list})
+
+def events(request,event_id):
+      '''
+      View root page function that returns the events page and its data
+      '''
+      try:
+            title = 'Events |'
+            event = Event.objects.get(id=event_id)
+      except Event.DoesNotExist:
+            raise Http404
+      return render(request, 'events.html', {"event":event})
