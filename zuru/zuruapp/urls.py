19effcf21d8256dc104fd6d0754726f7180e7ba7
cospamental <cospa18@gmail.com>
2018-03-22 18:34:44 +0300

    added event url

--- zuru/zuruapp/urls.py
+++ zuru/zuruapp/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.home,name='home'),
+    url('^event/(\d+)', views.events, name='events'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
